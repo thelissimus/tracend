5cbc282258126893c1c207acb704ff437ce25ac1
thelissimus <thelissimus@tuta.io>
2025-08-22 05:02:53 +0500

    add: hello world

--- src/main.zig
+++ src/main.zig
@@ -1,1 +1,32 @@
-pub fn main() !void {}
+const std = @import("std");
+
+pub fn main() !void {
+    var bw = std.io.bufferedWriter(std.io.getStdOut().writer());
+    const stdout = bw.writer();
+
+    const width: i32 = 256;
+    const height: i32 = 256;
+
+    try stdout.print(
+        \\P3
+        \\{d} {d}
+        \\255
+        \\
+    , .{ width, height });
+
+    for (0..height) |j| {
+        for (0..width) |i| {
+            const r: f64 = @as(f64, @floatFromInt(i)) / @as(f64, @floatFromInt(width - 1));
+            const g: f64 = @as(f64, @floatFromInt(j)) / @as(f64, @floatFromInt(height - 1));
+            const b: f64 = 0.0;
+
+            const ir: i32 = @intFromFloat(255.999 * r);
+            const ig: i32 = @intFromFloat(255.999 * g);
+            const ib: i32 = @intFromFloat(255.999 * b);
+
+            try stdout.print("{d} {d} {d}\n", .{ ir, ig, ib });
+        }
+    }
+
+    try bw.flush();
+}
